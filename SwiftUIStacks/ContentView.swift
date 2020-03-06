4cbb9e54dd13a48639fecf20b4ec49931f7ad14b
Sophia <gmb728@gmail.com>
2020-03-06 10:32:11 +0800

    init

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -8,14 +8,105 @@
 
 import SwiftUI
 
+ 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 20){
+           HeaderView()
+        HStack(alignment: .center, spacing: 20){
+            VStack(alignment: .center, spacing: 10){
+            Image("Lovey")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 170)
+                .cornerRadius(20)
+            .cornerRadius(20)
+                 .shadow(color: .gray, radius: 10, x: 0, y: 10)
+            Text("Lovey")
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(.pink)
+                .multilineTextAlignment(.trailing)
+            }
+            VStack(alignment: .center, spacing: 10){
+            Image("Fanboy")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 170)
+                .cornerRadius(20)
+                 .shadow(color: .gray, radius: 10, x: 0, y: 10)
+            Text("Fanboy")
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(.pink)
+                .multilineTextAlignment(.trailing)
+                }
+            }
+            HStack(alignment: .center, spacing: 20){
+                       VStack(alignment: .center, spacing: 10){
+                       Image("Crazy Eyes")
+                           .resizable()
+                           .scaledToFit()
+                           .frame(width: 170)
+                           .cornerRadius(20)
+                       .cornerRadius(20)
+                        .shadow(color: .gray, radius: 10, x: 0, y: 10)
+                       Text("Crazy Eyes")
+                           .font(.system(.title, design: .rounded))
+                           .fontWeight(.black)
+                           .foregroundColor(.pink)
+                           .multilineTextAlignment(.trailing)
+                       }
+                ZStack(alignment: .center){
+                VStack(alignment: .center, spacing: 10){
+                       Image("Lance")
+                           .resizable()
+                           .scaledToFit()
+                           .frame(width: 170)
+                           .cornerRadius(20)
+                         .shadow(color: .gray, radius: 10, x: 0, y: 10)
+                       Text("Lance")
+                           .font(.system(.title, design: .rounded))
+                           .fontWeight(.black)
+                           .foregroundColor(.pink)
+                           .multilineTextAlignment(.trailing)
+                           }
+                    Text("Pro")
+                        .font(.largeTitle)
+                        .foregroundColor(.yellow)
+                        .fontWeight(.black)
+                        .offset(x: 5, y: -20)
+                }
+                }
+                       }
+           
+ 
+    .padding(30)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+struct HeaderView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2){
+            
+            Text("Which Bird")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+             Text("Would You Like to Be?")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+        }
+        .shadow(color: .gray, radius: 10, x: 0, y: 10)
+    }
+}
+
+ 
+
+ 
+struct ContentPreviews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+ 
